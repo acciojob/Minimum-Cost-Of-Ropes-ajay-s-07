fb7e48cd5075cc5887d8a6b8ff4a6087a5d083f3
Ajay Sarkate <115990815+ajay-s-07@users.noreply.github.com>
2023-02-14 15:48:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ function minimumCost() {
     inputArray = inputArray.split(',').map(Number);
     for (let i = 0; i < inputArray.length - 1; i += 0) {
         if (inputArray.length == 0) break;
+		// sort by decreasing order
         inputArray = inputArray.sort((a,b) => b-a);
         res = inputArray.pop() + inputArray.pop();
         cost += res;
+		// adding res to arrayy
         inputArray[inputArray.length] = res;   
     }  
     document.getElementById("result").innerHTML = cost;
